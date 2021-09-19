56190293372a038185f83b65f3118823642d2ce4
Yurii <leoonovn@gmail.com>
2021-09-19 13:46:03 +0300

    master

---
+++ Modul5HW1/Modul5HW1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Modul5HW1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var serviceProvider = new ServiceCollection()
+                .AddTransient<Starter>()
+                .BuildServiceProvider();
+
+            var starter = serviceProvider.GetService<Starter>();
+            starter.Run();
+        }
+    }
+}
---
+++ Modul5HW1/Modul5HW1/Starter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul5HW1
+{
+    public class Starter
+    {
+        public void Run()
+        {
+            Console.WriteLine("master");
+        }
+    }
+}
